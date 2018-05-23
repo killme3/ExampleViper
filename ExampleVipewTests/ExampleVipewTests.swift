02fc2fe6caa2b214f07dc1fc20f5261bda3fb3d4
yana mulyana <fulan@bar0notwin0.iconpln.co.id>
2018-05-23 10:12:31 +0700

    Initial Commit

---
+++ ExampleVipewTests/ExampleVipewTests.swift
@@ -0,0 +1,36 @@
+//
+//  ExampleVipewTests.swift
+//  ExampleVipewTests
+//
+//  Created by yana mulyana on 23/05/18.
+//  Copyright © 2018 yana mulyana. All rights reserved.
+//
+
+import XCTest
+@testable import ExampleVipew
+
+class ExampleVipewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
